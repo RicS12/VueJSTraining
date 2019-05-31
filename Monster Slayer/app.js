0e694abd7fc8c1f20dde6cd4602b3a38c6aa5289
RicS <ric.sego.22@gmail.com>
2019-05-31 15:24:28 -0400

    Monster Slayer is finished

--- Monster Slayer/app.js
+++ Monster Slayer/app.js
@@ -3,17 +3,26 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
             //Dealing damage to the monster
-            this.monsterHealth -= this.calculateDamage(3,10);
+            let damage = this.calculateDamage(3,10);
+            this.monsterHealth -= damage;
+            //Adding turn
+            this.turns.unshift( { //adds as a first element, and shifts others to the right
+                isPlayer: true,
+                text: `Player hits the Monster for ${damage}`
+            }) 
+
             //checking if we won
             if (this.checkWin()) {//if this is true then we won
                 return; // so do not execute code anymore
@@ -23,7 +32,13 @@ new Vue({
         },
         specialAttack: function() {
             //Dealing damage to the monster
-            this.monsterHealth -= this.calculateDamage(10,20);
+            let damage = this.calculateDamage(10,20);
+            this.monsterHealth -= damage;
+
+            this.turns.unshift( {
+                isPlayer: true,
+                text: `Player hits the Monster hard for ${damage}`
+            }) 
             if (this.checkWin()) {
                 return; 
             }
@@ -35,14 +50,23 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift( {
+                isPlayer: true,
+                text: `Player heals for 10`
+            }) 
             this.monsterAttack();
         },
         giveUp: function() {
             this.gameIsRunning = false;
         },
         monsterAttack: function() {
-            this.playerHealth -= this.calculateDamage(5,12);
+            let damage = this.calculateDamage(5,12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage}`
+            })
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min); //this will return a number between min and max.
