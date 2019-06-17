b4e353e9fe44d42d50d38b0c5d3561965e341347
RicS <ric.sego.22@gmail.com>
2019-06-17 15:26:39 -0400

    Assignment done

--- Start/src/components/Servers.vue
+++ Start/src/components/Servers.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="col-xs-12 col-sm-6">
                 <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
+                    <app-server
+                    v-for="server in servers"
+                    :server="server">
+                    </app-server>
                 </ul>
             </div>
 </template>
 
 <script>
+import Server from './Server.vue';
 export default {
     data: function() {
         return {
@@ -21,6 +21,9 @@ export default {
                 {id: 4, status: 'Normal'}
             ]
         }
+    },
+    components: {
+        appServer: Server
     }
 }
 </script>
