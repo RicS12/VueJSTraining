6c51e9eec8a65eb14676bc5d52be66f351475d84
RicS <ric.sego.22@gmail.com>
2019-07-04 17:04:13 -0400

    Animation and transition update

--- Animations and transitions/src/App.vue
+++ Animations and transitions/src/App.vue
@@ -4,31 +4,151 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br><br>
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br><br>
                 <!-- You only animate 1 element with transition as you can only show 1 at a time -->
-                <transition name="fade">
+                <transition :name="alertAnimation">
                     <!-- you can also use v-show instead of v-if, but reminder that v-show doesn't remove -->
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
                 <!-- By adding "type", even if the length of the transition is longer than the animation, as animation ends earlier and dictates the length, transition also ends. I.e: animation lasts 1s, transition 3s. When 1s passes, this animation ends-->
-                <transition name="slide" type="animation">
+                <transition name="slide" type="animation" appear>
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
-                <transition name="fade" appear>
+                <!-- New transition css animation classes -->
+                <transition 
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake"
+                    >
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some warning</div>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    :css="false">
+                    <div style="width: 300px; height: 100px; background-color: lightgreen" 
+                    v-if="load"></div>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" 
+                @click="selectedComponent == 
+                'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'">Toggle Component</button>
+                <br><br>
+                <transition name="fade" mode="out-in">
+                    <component :is="selectedComponent"></component>
+                </transition>
+                <hr>
+                <button class="btn btn-primary" @click="addItem">Add Item</button>
+                <br><br>
+                <ul class="list-group">
+                    <transition-group name="slide">
+                        <li 
+                            class="list-group-item" 
+                            v-for="(number, index) in numbers"
+                            @click="removeItem(index)"
+                            style="cursor: pointer"
+                            :key="number"> {{ number }}                         
+                        </li>             
+                    </transition-group>      
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { setInterval, clearInterval } from 'timers';
+import DangerAlert from './DangerAlert.vue';
+import SuccessAlert from './SuccessAlert.vue';
+
     export default {
         data() {
             return {
-                show: false
+                show: false,
+                load: true,
+                alertAnimation: 'fade',
+                elementWidth: 100,
+                selectedComponent: 'app-success-alert',
+                numbers: [1, 2, 3, 4, 5]
             }
+        },
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter');
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+            },
+            enter(el, done) {
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(()=> {
+                    el.style.width = (this.elementWidth + round * 10) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterEnter(el) {
+                console.log('afterEnter');
+            },
+            enterCancelled(el){
+                console.log('enterCancelled');
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave');
+                this.elementWidth = 300;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el, done) {
+                console.log('leave');
+                 let round = 1;
+                const interval = setInterval(()=> {
+                    el.style.width = (this.elementWidth - round * 10) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterLeave(el) {
+                console.log('afterLeave');
+            },
+            leaveCancelled(el){
+                console.log('leaveCancelled');
+            },
+            addItem() {
+                const pos = Math.floor(Math.random() * this.numbers.length);
+                this.numbers.splice(pos, 0, this.numbers.length + 1);
+            },
+            removeItem(index) {
+                this.numbers.splice(index, 1);
+            }
+        },
+        components: {
+            appDangerAlert: DangerAlert,
+            appSuccessAlert: SuccessAlert
         }
     }
 </script>
@@ -40,7 +160,7 @@
     }
 /* This is where the animation truly takes place after the 1 frame */
     .fade-enter-active {
-        transition: opacity .5s;
+        transition: opacity 1s;
     }
 
     .fade-leave {
@@ -48,7 +168,7 @@
     }
 
     .fade-leave-active {
-        transition: opacity .5s;
+        transition: opacity 1s;
         opacity: 0;
     }
 
@@ -58,7 +178,7 @@
     }
 
     .slide-enter-active {
-        animation: slide-in .5s ease-out forwards;
+        animation: slide-in 1s ease-out forwards;
         transition: opacity .5s;
     }
 
@@ -67,9 +187,14 @@
     }
 
     .slide-leave-active {
-        animation: slide-out .5s ease-out forwards;
-        transition: opacity .5s;
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
         opacity: 0;
+        position: absolute;
+    }
+
+    .slide-move {
+        transition: transform 1s;
     }
 
     @keyframes slide-in {
