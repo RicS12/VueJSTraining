5223fca37a7727a017a1146b12e53e646b80ff5b
RicS <ric.sego.22@gmail.com>
2019-07-12 18:13:26 -0400

    some new routes added

--- Routing/src/routes.js
+++ Routing/src/routes.js
@@ -1,7 +1,19 @@
 import User from './components/user/User.vue';
 import Home from './components/Home.vue';
+import UserStart from './components/user/UserStart.vue';
+import UserDetail from './components/user/UserDetail.vue';
+import UserEdit from './components/user/UserEdit.vue';
+
 
 export const routes = [
     { path: '', component: Home},
-    { path: '/user', component: User}
+    //By adding :id, you make it so that the URL is set dynamically, in Header "id" is hardcoded to 10
+    // { path: '/user/:id', component: User}
+    { path: '/user', component: User, children: [
+        {path: '', component: UserStart},
+        {path: ':id', component: UserDetail},
+        {path: ':id/edit', component: UserEdit},
+
+    ]}
+
 ];
