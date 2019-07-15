5994b38499a1c6a85cd680bf4fe48ef359c39bc4
RicS <ric.sego.22@gmail.com>
2019-07-15 19:00:12 -0400

    more updates of routing

--- Routing/src/components/Header.vue
+++ Routing/src/components/Header.vue
@@ -4,7 +4,6 @@
             <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
             <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
             <!-- <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link> -->
-
         </ul>
     </div>
 </template>
--- Routing/src/routes.js
+++ Routing/src/routes.js
@@ -7,7 +7,7 @@ import UserEdit from './components/user/UserEdit.vue';
 
 export const routes = [
     { path: '', component: Home},
-    //By adding :id, you make it so that the URL is set dynamically, in Header "id" is hardcoded to 10
+    //By adding :id, you make it so that the URL is set dynamically, in Header.vue "id" is hardcoded to 10
     // { path: '/user/:id', component: User}
     { path: '/user', component: User, children: [
         {path: '', component: UserStart},
