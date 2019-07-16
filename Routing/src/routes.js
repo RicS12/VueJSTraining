855a5c5eef6d8dc4cbb5694df87aa793fb321408
RicS <ric.sego.22@gmail.com>
2019-07-16 18:27:05 -0400

    adding redirecting

--- Routing/src/routes.js
+++ Routing/src/routes.js
@@ -3,17 +3,28 @@ import Home from './components/Home.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
+import Header from './components/Header.vue';
+
 
 
 export const routes = [
-    { path: '', component: Home},
+    { path: '', name: 'home', components: { 
+        default: Home,
+        'header-top': Header
+    }},
     //By adding :id, you make it so that the URL is set dynamically, in Header.vue "id" is hardcoded to 10
     // { path: '/user/:id', component: User}
-    { path: '/user', component: User, children: [
+    { path: '/user', components: { 
+        default: User,
+        'header-bottom': Header
+    }, children: [
         {path: '', component: UserStart},
         {path: ':id', component: UserDetail},
-        {path: ':id/edit', component: UserEdit},
+        {path: ':id/edit', component: UserEdit, name: 'userEdit'},
 
-    ]}
+    ]},
+    { path: '/redirect-me', redirect: {name: 'home'}},
+    //Redirecting, star catches all possible routing names not included and redirects
+    { path: '*', redirect: '/'}
 
 ];
