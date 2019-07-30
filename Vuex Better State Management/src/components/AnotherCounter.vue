e0c00c04cad89541c312a45aadb73aecf74fb5be
RicS <ric.sego.22@gmail.com>
2019-07-30 14:22:37 -0400

    vuex module done

---
+++ Vuex Better State Management/src/components/AnotherCounter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 50, duration: 500})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 50, duration: 500})">Decrement</button>
+    </div>
+</template>
+
+<script>
+//Trying out mapActions
+import { mapActions } from 'vuex';
+
+    export default {
+        methods: {
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement'
+            ])
+
+            //WITHOUT USING MAPMUTATIONS, YOU HAVE TO USE COMMIT
+            // increment() {
+            //     this.$store.commit('increment');
+            // },
+            // decrement() {
+            //     this.$store.commit('decrement');
+            // }
+        }
+    }
+</script>
---
+++ Vuex Better State Management/src/components/AnotherResult.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of clicks: {{ stringCounter }}</p>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+    export default {
+        computed: {
+            //you have to install:npm install --save babel-preset-stage-2 to use "..."mapGetters
+            //I think @yarn add babel-preset-stage-2@ works too
+            //this allows to also use computed properties together with mapGetters
+            ...mapGetters([
+            'doubleCounter', 
+            'stringCounter' 
+            ])
+
+
+            //OLD WAY WITHOUT USING MAPGGETERS
+            // counter() {
+            //     return this.$store.getters.doubleCounter;
+            // },
+            // clicks() {
+            //     return this.$store.getters.stringCounter;
+            // }
+        }
+    }
+</script>
