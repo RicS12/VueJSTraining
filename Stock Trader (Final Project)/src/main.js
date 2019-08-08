9309e80ab7f901f30e7651dd0e28f7977df54017
RicS <ric.sego.22@gmail.com>
2019-08-08 18:28:50 -0400

    stock trader complete

--- Stock Trader (Final Project)/src/main.js
+++ Stock Trader (Final Project)/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import { routes } from './routes';
 //importing store (central state manager)
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-e8c08.firebaseio.com/';
+
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
 })
